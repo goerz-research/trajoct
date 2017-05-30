98e5b283513d1af725e1017851516d03a7f16cbd
Michael Goerz <goerz@stanford.edu>
2017-05-30 13:48:52 -0700

    Update cluster configuration

--- config/jupyter_notebook_config.py
+++ config/jupyter_notebook_config.py
@@ -14,7 +14,9 @@ def post_save(model, os_path, contents_manager):
         return  # only do this for notebooks
     d, fname = os.path.split(os_path)
     # check_call(['jupyter', 'nbconvert', '--to', 'script', fname], cwd=d)
-    check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
+    check_call(
+        ['jupyter', 'nbconvert', '--to', 'html', '--output-dir=html', fname],
+        cwd=d)
 
 
 c.NotebookApp.contents_manager_class = 'notedown.NotedownContentsManager'
